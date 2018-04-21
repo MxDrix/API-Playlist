c5a0440e1431400b892721c7f99055f0d22927dc
RICHARD\Richard <richrd.chandon@gmail.com>
2018-04-21 21:36:21 +0200

    fist routing

--- apiplaylist.js
+++ apiplaylist.js
@@ -9,13 +9,13 @@ var myRouter = express.Router();
  
 myRouter.route('/recherche/:id_api&:theme')
 .get(function(req,res){ 
-	  res.json({message : "Vous souhaitez accéder aux informations de la piscine n°" + req.params.id_api+" "+ req.params.theme});
+	  res.json({message : "Première recherche n°" + req.params.id_api+" "+ req.params.theme});
 })
 .put(function(req,res){ 
-	  res.json({message : "Vous souhaitez modifier les informations de la piscine n°" + req.params.piscine_id});
+	  res.json({message : "Modification " + req.params.id_api});
 })
 .delete(function(req,res){ 
-	  res.json({message : "Vous souhaitez supprimer la piscine n°" + req.params.piscine_id});
+	  res.json({message : "Suppression" + req.params.id_api});
 });
 app.use(myRouter);  
  
