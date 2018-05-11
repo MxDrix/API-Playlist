4f2b170ea8236d3b80e351c470c83bb4ad2f1104
RICHARD\Richard <richrd.chandon@gmail.com>
2018-05-11 15:13:05 +0200

    connexion + debut update user

--- Routes/route.js
+++ Routes/route.js
@@ -2,40 +2,47 @@
 var express = require('express'); 
 var mongoose = require('mongoose');
 var Users = require('../Models/model');
+var conn = mongoose.connection;
 const usersRoute = express.Router();
 usersRoute.route('/inscription/:nom&:prenom&:pseudo&:email&:password&:abonnement')
-    .get((req, res) => {
-        console.log(req.params.nom);
-        var conn = mongoose.connection;
+    .post((req, res) => {
         let user = new Users({nom: req.params.nom,prenom: req.params.prenom,pseudo: req.params.pseudo,email: req.params.email,password: req.params.password,abonnement: req.params.abonnement});
         user.save();
         var query  = Users.where({email :req.params.email});
         query.findOne(function (err, result) {
             if (err) return handleError(err);
             if (result) {
+                res.status(400).send("This user email already existe");
             }else{         
                 conn.collection('utilisateur').insert(user);
-                res.status(201).send(user) 
+                res.status(201).send(user); 
             }
         });
     })
-    .post((req, res) => {
-        let user = new Users(req.params.nom,req.params.prenom,req.params.pseudo,req.params.email,req.params.abonnement);
-        user.save();
-        res.status(201).send(user) 
-    })
+
     usersRoute.route('/connexion/:pseudo&:password')
     .get((req, res) => {
-        console.log(req.params.pseudo);
-        var conn = mongoose.connection;
         var query  = Users.where({pseudo :req.params.pseudo,password:req.params.password});
         query.findOne(function (err, result) {
             if (err) return handleError(err);
             if (result) {
-                console.log("Connexion ok");
+                res.status(200).send(result);
             }else{
-                console.log("This username doesn't existe");
+                res.status(400).send("Incorrect pseudo or password");
             }
         });        
+    })
+    
+    usersRoute.route('/update/:email&:nom&:prenom&:pseudo&:password&:abonnement')
+    .post((req, res) => {
+        try {
+            conn.collection('utilisateur').updateOne(
+               { "email" : req.params.email },
+               { $set: {"nom" : req.param.nom, "prenom" : req.param.prenom, "pseudo" : req.param.pseudo,"password" : req.param.password, "abonnement" : req.param.abonnement } },
+               { upsert: true }
+            );
+         } catch (e) {
+            print(e);
+         }       
     })
 module.exports = usersRoute;
