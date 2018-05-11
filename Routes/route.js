e449b174a39c3ce6a5282a7031a27a7896cc00a3
RICHARD\Richard <richrd.chandon@gmail.com>
2018-05-11 16:49:17 +0200

    connexion + inscription + update + delete + status : ok

--- Routes/route.js
+++ Routes/route.js
@@ -3,9 +3,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var Users = require('../Models/model');
 var conn = mongoose.connection;
+var collection = conn.collection('utilisateur');
 const usersRoute = express.Router();
+
 usersRoute.route('/inscription/:nom&:prenom&:pseudo&:email&:password&:abonnement')
-    .post((req, res) => {
+    .put((req, res) => {
         let user = new Users({nom: req.params.nom,prenom: req.params.prenom,pseudo: req.params.pseudo,email: req.params.email,password: req.params.password,abonnement: req.params.abonnement});
         user.save();
         var query  = Users.where({email :req.params.email});
@@ -14,12 +16,11 @@ usersRoute.route('/inscription/:nom&:prenom&:pseudo&:email&:password&:abonnement
             if (result) {
                 res.status(400).send("This user email already existe");
             }else{         
-                conn.collection('utilisateur').insert(user);
+                collection.insert(user);
                 res.status(201).send(user); 
             }
         });
-    })
-
+    });
     usersRoute.route('/connexion/:pseudo&:password')
     .get((req, res) => {
         var query  = Users.where({pseudo :req.params.pseudo,password:req.params.password});
@@ -31,18 +32,36 @@ usersRoute.route('/inscription/:nom&:prenom&:pseudo&:email&:password&:abonnement
                 res.status(400).send("Incorrect pseudo or password");
             }
         });        
-    })
-    
+    });    
     usersRoute.route('/update/:email&:nom&:prenom&:pseudo&:password&:abonnement')
-    .post((req, res) => {
+    .put((req, res) => {
+        var query = { email: req.params.email};
+        collection.findOneAndUpdate(query, { $set: {  
+            nom: req.params.nom,
+            prenom: req.params.prenom,
+            pseudo: req.params.pseudo,
+            password: req.params.password,
+            abonnement: req.params.abonnement   
+        }}, {returnOriginal: false}, function(err, doc){
+            if(err){
+                console.log("Something wrong when updating data!");
+            }
+            if(doc){
+                res.status(200).send(doc);
+            }
+        });
+    })
+    usersRoute.route('/delete/:email')
+    .delete((req, res) => {
         try {
-            conn.collection('utilisateur').updateOne(
-               { "email" : req.params.email },
-               { $set: {"nom" : req.param.nom, "prenom" : req.param.prenom, "pseudo" : req.param.pseudo,"password" : req.param.password, "abonnement" : req.param.abonnement } },
-               { upsert: true }
-            );
-         } catch (e) {
-            print(e);
-         }       
+            var query = { email: req.params.email};
+            collection.findOneAndDelete(query, function(err) {                
+                if (err) return res.status(500).send(err);
+                return res.status(200).send("User delete");
+            });
+        }
+        catch(e){
+            res.status(400).send(e);
+        }
     })
 module.exports = usersRoute;
