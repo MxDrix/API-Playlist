9b0c5dee14d165cbd477dcc0f736a9a9b44a9388
Benjamin NGOUM <benjamin.ngoum@gmail.com>
2018-05-13 00:09:59 +0200

    Add schema model about Playlist

--- Models/model.js
+++ Models/model.js
@@ -12,6 +12,47 @@ const Users = new Schema({
 	password: { type: String },
 	abonnement: { type: String }
 });
+
+const Categories = new Schema({
+	id: { type: String, default: [ "business", "entertainment", "health", "science", "sports", "technology"] }
+})
+
+const Sources = new Schema({
+	id: String,
+	nom: String
+})
+
+const Celebrites = new Schema({
+	nom: String
+})
+
+const MotsCle = new Schema({
+	libelle: String
+})
+
+const Playlists = new Schema({
+	id_playlist: Number,
+	id_user: Number,
+	nom: String,
+	content: [
+		{
+			Categories: {
+				id: String
+			},
+			Sources: {
+				id: String,
+				nom: String
+			},
+			Celebrites: {
+				nom: String
+			},
+			MotsCle: {
+				libelle: String
+			}
+		}
+	]
+})
+
 Users.method('insertOne', function(user) {
     collection.insert(user);
     return user; 
