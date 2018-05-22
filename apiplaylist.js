adefa34b3b4d5e0dbf467e33e117eb21e283d153
Benjamin NGOUM <benjamin.ngoum@gmail.com>
2018-05-22 17:21:25 +0200

    Modif Concurrently

--- apiplaylist.js
+++ apiplaylist.js
@@ -170,7 +170,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // API routes
 app.use('/api', usersRoute);
-app.post('/api/playlist/create', createPlaylistLimiter);
+app.post('/api/playlist/create');
 
 app.get('/', function (req, res) {
 	res.send('API Playlist Veille Informationnelle');
