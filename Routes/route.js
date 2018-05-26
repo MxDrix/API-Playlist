9fe531dc8b9353f8b62c484c5a9331031dfe2a4a
MxDrix <richard.chandon@gmail.com>
2018-05-26 21:08:08 +0200

    test routeemail

--- Routes/route.js
+++ Routes/route.js
@@ -111,7 +111,10 @@ app.route('/connexion')
                                         allResponse.sort(function(a, b){
                                             return a.publishedAt - b.publishedAt;
                                         });
-                                        res.status(200).json({ all: {User: {nom: result.nom, prenom: result.prenom}, abo: allAbonnements, news: allResponse}});
+                                        res.status(200).json({ all: {
+                                            User: {nom: result.nom, prenom: result.prenom, pseudo: result.pseudo,email: result.email,dateinscription: result.dateinscription }, 
+                                            abo: allAbonnements, 
+                                            news: allResponse}});
                                     }
                                 });
                             }
@@ -129,6 +132,47 @@ app.route('/connexion')
                
     });
 
+    const nodemailer = require('nodemailer');
+app.route('/email')
+    .get((req, res)=> {
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: 'richard.chandon@gmail.com, baz@example.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
+    });
 // Update user by email
 app.route('/update')
     .put((req, res) => {
